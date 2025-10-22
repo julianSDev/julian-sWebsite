14b6c8297200b77f51ad0aff50757a47d00d5068
julianSDev <julian.sch.dev@gmail.com>
2025-10-22 11:22:47 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+### Description
+Julian portfolio Website for developer/designers built with React. 
+
---
+++ src/content_option.js
@@ -0,0 +1,184 @@
+const logotext = "Julian";
+const meta = {
+    title: "Julian Schmidt",
+    description: "I’m Julian Schmidt Unity Game _ Full stack devloper, currently working in Canada",
+};
+
+const introdata = {
+    title: "I’m Julian Schmidt",
+    animated: {
+        first: "I love coding",
+        second: "Web using MERN stack",
+        third: "Game apps using Unity",
+    },
+    description: `I’m a passionate Full-_Stack Developer with over 8 years of experience in the MERN stack, 3 years working with Unity, and 4 years specializing in Web3.js and blockchain integration.
+    With a deep understanding of both traditional and emerging technologies, I love solving complex problems and pushing the boundaries of what’s possible on the web and beyond.`,
+    your_img_url: "https://plus.unsplash.com/premium_photo-1661609716359-307bcff7ae87?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE2NHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=60&w=500",
+};
+
+const dataabout = {
+    title: "A Bit About Myself",
+    aboutme: "I’m a results-driven Full Stack and Blockchain Developer with a passion for creating innovative, reliable, and scalable digital solutions. I value clear communication, organized workflows, and continuous learning. My goal is to build meaningful projects and long-term collaborations in the global tech community, bridging creativity with technical excellence.",
+};
+
+const worktimeline = [{
+    jobtitle: "Full Stack Developer ",
+    where: "Commitsmart",
+    date: "2024",
+},
+{
+    jobtitle: "Lead Developer",
+    where: "YourLife",
+    date: "2023",
+},
+{
+    jobtitle: "Senior Frontend Developer",
+    where: "BlockBasis",
+    date: "2022",
+},
+];
+
+const skills = [{
+    name: "MERN",
+    value: 95,
+},
+{
+    name: "TypeScript",
+    value: 85,
+},
+{
+    name: "Tailwind",
+    value: 85,
+},
+{
+    name: "C#",
+    value: 90,
+},
+{
+    name: "Unity",
+    value: 85,
+},
+{
+    name: "Web3",
+    value: 60,
+},
+];
+
+const services = [
+    {
+        title: "Web Development",
+        description:
+            "Building robust, scalable, and high-performance web applications using modern frameworks and best development practices. I specialize in delivering end-to-end solutions—from front-end interfaces to secure, efficient back-end systems.",
+    },
+    {
+        title: "DApps",
+        description:
+            "Designing and developing decentralized applications (DApps) with secure smart contracts, optimized gas usage, and seamless blockchain integration. Experienced with Ethereum, Polygon, BNB Chain, and Web3.js frameworks.",
+    },
+    {
+        title: "Unity Game Development",
+        description:
+            "Creating immersive 2D and 3D game experiences using Unity. Skilled in gameplay programming, optimization, UI/UX design, and integrating multiplayer or blockchain features for next-generation interactive content.",
+    },
+];
+
+const dataportfolio = [
+    {
+        img: "https://plus.unsplash.com/premium_photo-1675083923190-387d05e8fe12?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8ODl8fHdlYjN8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&q=60&w=500",
+        description:
+            "NEAR Protocol — A high-performance blockchain built for scalable, agent-driven applications and modular infrastructure.",
+        link: "https://near.org/",
+    },
+    {
+        img: "https://images.unsplash.com/photo-1676907820153-2b61de2b9daf?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8d2ViM3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&q=60&w=500",
+        description:
+            "Celestia — Modular blockchain network enabling developers to deploy independent chains with full-stack control of data and execution layers.",
+        link: "https://celestia.org/",
+    },
+    {
+        img: "https://plus.unsplash.com/premium_photo-1682124732859-ee0f19b31d31?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDIwfHx8ZW58MHx8fHx8&auto=format&fit=crop&q=60&w=500",
+        description:
+            "Blockchain.com — Crypto infrastructure & wallet service providing explorer tools, APIs and institutional-grade crypto solutions.",
+        link: "https://www.blockchain.com/",
+    },
+    {
+        img: "https://images.unsplash.com/photo-1692211813378-1a906240caaf?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDEwfHx8ZW58MHx8fHx8&auto=format&fit=crop&q=60&w=500",
+        description:
+            "Decentraland — A Web3 virtual world where users can explore, create and transact using blockchain-based assets in a shared metaverse.",
+        link: "https://decentraland.org/",
+    },
+    {
+        img: "https://images.unsplash.com/photo-1639754391620-197afd388802?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fGJsb2NrY2hhaW58ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&q=60&w=500",
+        description:
+            "“Blockchain Demo” — Interactive browser app visualising blockchain blocks, hashing and transactions.",
+        link: "https://andersbrownworth.com/blockchain/",
+    },
+    {
+        img: "https://fastly.picsum.photos/id/661/400/700.jpg?grayscale&hmac=HewWDSeKJCPDGKW1ewWBKStVQ73YGFwv1JokTJcVfso",
+        description:
+            "“Strapi” — Open-source headless CMS built with Node.js. Enables fast API generation (REST/GraphQL) for modern web applications and is widely adopted by developers.",
+        link: "https://strapi.io/",
+    },
+    {
+        img: "https://plus.unsplash.com/premium_photo-1677269465314-d5d2247a0b0c?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=687",
+        description:
+            "“Zama Protocol Demo” — Demonstrates confidential token transfers and on-chain FHE (Fully Homomorphic Encryption) powered DeFi primitives.",
+        link: "https://zama.ai/",
+    },
+    {
+        img: "https://plus.unsplash.com/premium_photo-1680608979589-e9349ed066d5?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=764",
+        description:
+            "“Blockchain Demo 2.0” — A guided visual walkthrough of how cryptocurrencies and blocks work",
+        link: "https://blockchaindemo.io/",
+    },
+    {
+        img: "https://plus.unsplash.com/premium_photo-1682464651166-56902c08840c?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE0MHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=60&w=500",
+        description:
+            "“OpenProject” — Open-source project management platform (classic, agile & hybrid workflows).",
+        link: "https://www.openproject.org/",
+    },
+    {
+        img: "https://images.unsplash.com/photo-1710993011836-108ba89ebe51?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1170",
+        description:
+            "“Holochain” — Distributed application framework combining p2p networking with cryptographic validation (beyond traditional blockchain).",
+        link: "https://holochain.org/",
+    },
+    {
+        img: "https://plus.unsplash.com/premium_photo-1668234694846-c751b842b220?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDR8fHxlbnwwfHx8fHw%3D&auto=format&fit=crop&q=60&w=500",
+        description: "The wisdom of life consists in the elimination of non-essentials.", link: "#",
+    },
+    {
+        img: "https://images.unsplash.com/photo-1593508512255-86ab42a8e620?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1756",
+        description: "The wisdom of life consists in the elimination of non-essentials.", link: "#",
+    },];
+
+const contactConfig = {
+    YOUR_EMAIL: "julian.sch.dev@gmail.com",
+    YOUR_FONE: "+12232177508",
+    YOUR_TG: "@julianDev0216",
+    description: "",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/julianSDev",
+    linkedin: "https://linkedin.com",
+    // facebook: "https://facebook.com",
+    // twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
